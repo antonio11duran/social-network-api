501c611d511cc37606358c9a8124d7faf9661bf8
Duran11 <antonio11duran@gmail.com>
2022-12-19 12:22:47 -0700

    config initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const db = require("");
-const routes = require("");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const cwd = process.cwd();
 
