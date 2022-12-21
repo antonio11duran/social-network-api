686a31d7b34fdb542fde9c3139e2ce960c237d20
Duran11 <antonio11duran@gmail.com>
2022-12-21 09:33:34 -0700

    added updateUser route, added thought routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -92,4 +92,17 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
